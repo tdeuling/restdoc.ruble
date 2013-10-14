02f889aceb4ec366b07b942030d65bade28e1504
Thomas Deuling <info@coding.ms>
2013-10-14 12:22:10 +0200

    Code, Headline 1, Headline 2, Headline 3 Snippet added

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ fluid.ruble
 Aptana Studio 3 - reST Bundle for TYPO3 CMS, Neos and Flow Documentation - http://coding.ms 
 By Thomas Deuling
 www.coding.ms
+
+Links
+http://sphinx-doc.org/markup/inline.html#ref-role
--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -18,8 +18,73 @@
 # reST include file
 #####################################
 snippet t(:restIncludeFile) do |snip|
-  snip.trigger = "restIncludeFile"
-  snip.expansion = "sdfghjk"
+  snip.trigger = "..include"
+  snip.expansion = ".. include:: ${1:../Readme.rst}"
   snip.category = "reST-Documentation"
 end
+
+
+# reST message
+#####################################
+snippet t(:restMessage) do |snip|
+  snip.trigger = "..note"
+  snip.expansion = ".. ${1:note/important/tip/warning}:: $0"
+  snip.category = "reST-Documentation"
+end
+
+
+# reST code
+#####################################
+snippet t(:restCode) do |snip|
+  snip.trigger = "..code"
+  snip.expansion = ".. code-block:: ${1:ts/html/php/css/js}\n"
+  snip.expansion+= "    \n"
+  snip.expansion+= "    $0\n"
+  snip.expansion+= "    \n"
+  snip.category = "reST-Documentation"
+end
+
+
+# reST ToDo
+#####################################
+snippet t(:restToDo) do |snip|
+  snip.trigger = "..todo"
+  snip.expansion = ".. TODO:\n"
+  snip.expansion+= "    @todo: $0"
+  snip.category = "reST-Documentation"
+end
+
+
+# reST h1
+#####################################
+snippet t(:restH1) do |snip|
+  snip.trigger = "..h1"
+  snip.expansion = "====================================================================\n"
+  snip.expansion+= "$0\n"
+  snip.expansion+= "====================================================================\n"
+  snip.category = "reST-Documentation"
+end
+
+
+# reST h1
+#####################################
+snippet t(:restH2) do |snip|
+  snip.trigger = "..h2"
+  snip.expansion = "--------------------------------------------------------------------\n"
+  snip.expansion+= "$0\n"
+  snip.expansion+= "--------------------------------------------------------------------\n"
+  snip.category = "reST-Documentation"
+end
+
+
+# reST h1
+#####################################
+snippet t(:restH3) do |snip|
+  snip.trigger = "..h13"
+  snip.expansion = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+  snip.expansion+= "$0\n"
+  snip.expansion+= "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+  snip.category = "reST-Documentation"
+end
+ 
  
